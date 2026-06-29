7a7119c607a540a63e2e4f2781dc3f77a12bd018
jgatetekez-code <jgatetekez@alustudent.com>
2026-06-29 08:44:37 -0700

    switch script

--- python-data_structures/12-switch.py
+++ python-data_structures/12-switch.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    del my_list[idx]
-    return my_list
+a = 1
+b = 2
+a, b = b, a
+print("{:d} {:d}".format(a, b))
